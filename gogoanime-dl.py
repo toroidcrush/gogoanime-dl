9b19311adacc096484063921d543639af57dd23d
toroidcrush <97044706+toroidcrush@users.noreply.github.com>
2022-01-06 15:35:35 -0500

    Version 2.0.0
    Added error logs for debugging, fixed episode range logic.

--- gogoanime-dl.py
+++ gogoanime-dl.py
@@ -5,7 +5,7 @@ from tqdm import tqdm
 from re import compile
 import concurrent.futures
 
-headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:95.0) Gecko/20100101 Firefox/95.0'}
 
 
 def make_path(filename):
@@ -25,16 +25,28 @@ def get_saved_res_text(filename):
 def get_url_for_mp4(url_to_ep, resolution):
 
   res = requests.get(url=url_to_ep, headers=headers).text
-  link = BeautifulSoup(res, 'html.parser').find("li", class_="dowloads").find('a').get('href', None)
+  try:
+    dl_selection_link = BeautifulSoup(res, 'html.parser').find("li", class_="dowloads").find('a').get('href', None)
+  except:
+    return None
   
   # https://gogoplay1.com/download?id=MTA4MzY2&typesub=Gogoanime-DUB&title=Toriko+%28Dub%29+Episode+1
-  if not link or 'gogo' not in link or 'download' not in link: raise ValueError(link)
+  if not dl_selection_link or 'gogo' not in dl_selection_link or 'download' not in dl_selection_link:
+    with open(make_path('error.log'), mode='w') as f:
+      f.write(dl_selection_link + '\n\n' + res)
+    raise ValueError(f"Check {make_path('error.log')} for details")
 
-  res = requests.get(url=link, headers=headers).text
-  link = BeautifulSoup(res, 'html.parser').find(class_='dowload', text=compile('.*' + resolution + 'P.*')).find('a').get('href', None)
+  res = requests.get(url=dl_selection_link, headers=headers).text
+  try:
+    link = BeautifulSoup(res, 'html.parser').find(class_='dowload', text=compile('.*' + resolution + 'P.*')).find('a').get('href', None)
+  except:
+    return None
 
   # https://vidstreamingcdn.com/cdn23/42af3549e7b9a129b2bd0bd84a5505df/EP.1.v1.720p.mp4?mac=diPDSnLMzpQbwZ%2Fscejy2qqr3KX%2F1%2Fp4i3EcU97s%2BBo%3D&expiry=1641184365227
-  if not link or resolution not in link or 'mp4' not in link: raise ValueError(link)
+  if not link or resolution not in link or '.mp4?' not in link:
+    with open(make_path('error.log'), mode='w') as f:
+      f.write(dl_selection_link + '\n\n' + link + '\n\n' + res)
+    raise ValueError(f"Check {make_path('error.log')} for details")
 
   return link
 
@@ -45,11 +57,13 @@ def download_mp4(mp4_url, save_as):
       for chunk in tqdm(res.iter_content(chunk_size=1024), desc=save_as):
           if chunk:
               f.write(chunk)
-
+  return f'Downloaded: {mp4_url}'
 
 def get_episode(url_to_ep, resolution, save_as):
   mp4_link = get_url_for_mp4(url_to_ep, resolution)
-  download_mp4(mp4_link, save_as)
+  if mp4_link is None:
+    return f'Couldn\'t get link for {url_to_ep}'
+  return download_mp4(mp4_link, save_as)
 
 
 def main():
@@ -60,11 +74,11 @@ def main():
   # https://www2.gogoanime.cm/girls-und-panzer-dub-episode-1
   # https://www2.gogoanime.cm/girls-und-panzer-dub-episode-{nth_ep}
 
-  url_to_nth_ep = 'https://www2.gogoanime.cm/girls-und-panzer-dub-episode-{nth_ep}'
-  min_ep = 1
-  max_ep = 12
+  url_to_nth_ep = 'https://www3.gogoanime.cm/kakegurui-dub-episode-{nth_ep}'
+  min_ep = 0
+  max_ep = 2
   resolution = 720
-  save_directory = r"D:\girls_und_panzer"
+  save_directory = r"D:\kakegurui"
 
   multithreading = True
   workers = 5
@@ -72,6 +86,7 @@ def main():
   # --- do not modify below here --- #
 
   resolution = str(resolution)
+  os.makedirs(save_directory, exist_ok=True)
 
   if not multithreading:
     for i in range(min_ep, max_ep + 1):
@@ -99,10 +114,9 @@ def main():
         future_to_url[executor.submit(get_episode, url_to_ep, resolution, save_as)] = url_to_ep
         
       for future in concurrent.futures.as_completed(future_to_url):
-        url = future_to_url[future]
-        print('Downloaded: ' + url + '\n')
+        # url = future_to_url[future]
+        print(future.result() + '\n')
 
 
 if __name__=='__main__':
   main()
-
